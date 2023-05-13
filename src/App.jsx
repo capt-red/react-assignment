34c7bc19e894517bbb117568cf779c507c88eedd
Varun Dunna <95626468+capt-red@users.noreply.github.com>
2023-05-13 10:58:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 /*
  * Temporary problems array schema
  */
-const problems = [{
-    title: "201. Bitwise AND of Numbers Range",
-    difficulty: "Medium",
-    acceptance: "42%"
-},{
-    title: "201. Bitwise AND of Numbers Range",
-    difficulty: "Medium",
-    acceptance: "412%"
-},
-    {
-        title: "202. Happy Number",
-        difficulty: "Easy",
-        acceptance: "54.9%"
-    },
-    {
-        title: "203. Remove Linked List Elements",
-        difficulty: "Hard",
-        acceptance: "42%"
-    }];
-
-
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import Login from '../components/Login';
+import Signup from '../components/Signup';
+import Problemsetall from '../components/Problemsetall';
+import Problem from '../components/Problem';
+import Home from '../components/Home';
+import './App.css'
 function App() {
 
     /* Add routing here, routes look like -
@@ -29,31 +16,32 @@ function App() {
        /signup - Signup page
        /problemset/all/ - All problems (see problems array above)
        /problems/:problem_slug - A single problem page
-     */
+     
 
     return (
     <div>
         Finish the assignment! Look at the comments in App.jsx as a starting point
     </div>
-  )
+  )*/
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/problems" element={<Problemsetall/>} />
+        <Route path="problems/:slug" element={<Problem/>} />
+      </Routes>
+    </Router>
+  );
+  
 }
 
-// A demo component
-function ProblemStatement(props) {
-    const title = props.title;
-    const acceptance = props.acceptance;
-    const difficulty = props.difficulty;
 
-    return <tr>
-        <td>
-            {title}
-        </td>
-        <td>
-            {acceptance}
-        </td>
-        <td>
-            {difficulty}
-        </td>
-    </tr>
-}
-export default App
+
+  
+
+
+
+
+export default App;
